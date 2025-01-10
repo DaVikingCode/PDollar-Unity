9ddc4a91abcacfed7b7f4504ad97147167c30eb7
Aymeric <aymeric.lamboley@gmail.com>
2025-01-10 10:14:00 +0100

    updated to Unity 2022

--- Assets/PDollar/Scripts/Demo.cs
+++ Assets/PDollar/Scripts/Demo.cs
@@ -4,128 +4,137 @@ using System.Collections;
 using System.Collections.Generic;
 using System.IO;
 
-using PDollarGestureRecognizer;
+namespace PDollarGestureRecognizer
+{
+	public class Demo : MonoBehaviour
+	{
+		public Transform gestureOnScreenPrefab;
 
-public class Demo : MonoBehaviour {
+		private List<Gesture> trainingSet = new List<Gesture>();
 
-	public Transform gestureOnScreenPrefab;
+		private List<Point> points = new List<Point>();
+		private int strokeId = -1;
 
-	private List<Gesture> trainingSet = new List<Gesture>();
+		private Vector3 virtualKeyPosition = Vector2.zero;
+		private Rect drawArea;
 
-	private List<Point> points = new List<Point>();
-	private int strokeId = -1;
+		private RuntimePlatform platform;
+		private int vertexCount = 0;
 
-	private Vector3 virtualKeyPosition = Vector2.zero;
-	private Rect drawArea;
+		private List<LineRenderer> gestureLinesRenderer = new List<LineRenderer>();
+		private LineRenderer currentGestureLineRenderer;
 
-	private RuntimePlatform platform;
-	private int vertexCount = 0;
+		//GUI
+		private string message;
+		private bool recognized;
+		private string newGestureName = "";
 
-	private List<LineRenderer> gestureLinesRenderer = new List<LineRenderer>();
-	private LineRenderer currentGestureLineRenderer;
+		void Start()
+		{
+			platform = Application.platform;
+			drawArea = new Rect(0, 0, Screen.width - Screen.width / 3, Screen.height);
 
-	//GUI
-	private string message;
-	private bool recognized;
-	private string newGestureName = "";
+			//Load pre-made gestures
+			TextAsset[] gesturesXml = Resources.LoadAll<TextAsset>("GestureSet/10-stylus-MEDIUM/");
+			foreach (TextAsset gestureXml in gesturesXml)
+				trainingSet.Add(GestureIO.ReadGestureFromXML(gestureXml.text));
 
-	void Start () {
-
-		platform = Application.platform;
-		drawArea = new Rect(0, 0, Screen.width - Screen.width / 3, Screen.height);
-
-		//Load pre-made gestures
-		TextAsset[] gesturesXml = Resources.LoadAll<TextAsset>("GestureSet/10-stylus-MEDIUM/");
-		foreach (TextAsset gestureXml in gesturesXml)
-			trainingSet.Add(GestureIO.ReadGestureFromXML(gestureXml.text));
-
-		//Load user custom gestures
-		string[] filePaths = Directory.GetFiles(Application.persistentDataPath, "*.xml");
-		foreach (string filePath in filePaths)
-			trainingSet.Add(GestureIO.ReadGestureFromFile(filePath));
-	}
-
-	void Update () {
-
-		if (platform == RuntimePlatform.Android || platform == RuntimePlatform.IPhonePlayer) {
-			if (Input.touchCount > 0) {
-				virtualKeyPosition = new Vector3(Input.GetTouch(0).position.x, Input.GetTouch(0).position.y);
-			}
-		} else {
-			if (Input.GetMouseButton(0)) {
-				virtualKeyPosition = new Vector3(Input.mousePosition.x, Input.mousePosition.y);
-			}
+			//Load user custom gestures
+			string[] filePaths = Directory.GetFiles(Application.persistentDataPath, "*.xml");
+			foreach (string filePath in filePaths)
+				trainingSet.Add(GestureIO.ReadGestureFromFile(filePath));
 		}
 
-		if (drawArea.Contains(virtualKeyPosition)) {
+		void Update()
+		{
 
-			if (Input.GetMouseButtonDown(0)) {
+			if (platform == RuntimePlatform.Android || platform == RuntimePlatform.IPhonePlayer)
+			{
+				if (Input.touchCount > 0)
+				{
+					virtualKeyPosition = new Vector3(Input.GetTouch(0).position.x, Input.GetTouch(0).position.y);
+				}
+			}
+			else
+			{
+				if (Input.GetMouseButton(0))
+				{
+					virtualKeyPosition = new Vector3(Input.mousePosition.x, Input.mousePosition.y);
+				}
+			}
 
-				if (recognized) {
+			if (drawArea.Contains(virtualKeyPosition))
+			{
+				if (Input.GetMouseButtonDown(0))
+				{
+					if (recognized)
+					{
+						recognized = false;
+						strokeId = -1;
 
-					recognized = false;
-					strokeId = -1;
+						points.Clear();
 
-					points.Clear();
+						foreach (LineRenderer lineRenderer in gestureLinesRenderer)
+						{
 
-					foreach (LineRenderer lineRenderer in gestureLinesRenderer) {
+							lineRenderer.positionCount = 0;
+							Destroy(lineRenderer.gameObject);
+						}
 
-						lineRenderer.SetVertexCount(0);
-						Destroy(lineRenderer.gameObject);
+						gestureLinesRenderer.Clear();
 					}
 
-					gestureLinesRenderer.Clear();
+					++strokeId;
+
+					Transform tmpGesture = Instantiate(gestureOnScreenPrefab, transform.position, transform.rotation) as Transform;
+					currentGestureLineRenderer = tmpGesture.GetComponent<LineRenderer>();
+
+					gestureLinesRenderer.Add(currentGestureLineRenderer);
+
+					vertexCount = 0;
 				}
 
-				++strokeId;
-				
-				Transform tmpGesture = Instantiate(gestureOnScreenPrefab, transform.position, transform.rotation) as Transform;
-				currentGestureLineRenderer = tmpGesture.GetComponent<LineRenderer>();
-				
-				gestureLinesRenderer.Add(currentGestureLineRenderer);
-				
-				vertexCount = 0;
-			}
-			
-			if (Input.GetMouseButton(0)) {
-				points.Add(new Point(virtualKeyPosition.x, -virtualKeyPosition.y, strokeId));
+				if (Input.GetMouseButton(0))
+				{
+					points.Add(new Point(virtualKeyPosition.x, -virtualKeyPosition.y, strokeId));
 
-				currentGestureLineRenderer.SetVertexCount(++vertexCount);
-				currentGestureLineRenderer.SetPosition(vertexCount - 1, Camera.main.ScreenToWorldPoint(new Vector3(virtualKeyPosition.x, virtualKeyPosition.y, 10)));
+					currentGestureLineRenderer.positionCount = ++vertexCount;
+					currentGestureLineRenderer.SetPosition(vertexCount - 1, Camera.main.ScreenToWorldPoint(new Vector3(virtualKeyPosition.x, virtualKeyPosition.y, 10)));
+				}
 			}
 		}
-	}
 
-	void OnGUI() {
+		void OnGUI()
+		{
+			GUI.Box(drawArea, "Draw Area");
 
-		GUI.Box(drawArea, "Draw Area");
+			GUI.Label(new Rect(10, Screen.height - 40, 500, 50), message);
 
-		GUI.Label(new Rect(10, Screen.height - 40, 500, 50), message);
+			if (GUI.Button(new Rect(Screen.width - 100, 10, 100, 30), "Recognize"))
+			{
+				recognized = true;
 
-		if (GUI.Button(new Rect(Screen.width - 100, 10, 100, 30), "Recognize")) {
+				Gesture candidate = new Gesture(points.ToArray());
+				Result gestureResult = PointCloudRecognizer.Classify(candidate, trainingSet.ToArray());
 
-			recognized = true;
+				message = gestureResult.GestureClass + " " + gestureResult.Score;
+			}
 
-			Gesture candidate = new Gesture(points.ToArray());
-			Result gestureResult = PointCloudRecognizer.Classify(candidate, trainingSet.ToArray());
-			
-			message = gestureResult.GestureClass + " " + gestureResult.Score;
-		}
+			GUI.Label(new Rect(Screen.width - 200, 150, 70, 30), "Add as: ");
+			newGestureName = GUI.TextField(new Rect(Screen.width - 150, 150, 100, 30), newGestureName);
 
-		GUI.Label(new Rect(Screen.width - 200, 150, 70, 30), "Add as: ");
-		newGestureName = GUI.TextField(new Rect(Screen.width - 150, 150, 100, 30), newGestureName);
+			if (GUI.Button(new Rect(Screen.width - 50, 150, 50, 30), "Add") && points.Count > 0 && newGestureName != "")
+			{
+				string fileName = String.Format("{0}/{1}-{2}.xml", Application.persistentDataPath, newGestureName, DateTime.Now.ToFileTime());
 
-		if (GUI.Button(new Rect(Screen.width - 50, 150, 50, 30), "Add") && points.Count > 0 && newGestureName != "") {
-
-			string fileName = String.Format("{0}/{1}-{2}.xml", Application.persistentDataPath, newGestureName, DateTime.Now.ToFileTime());
-
-			#if !UNITY_WEBPLAYER
+#if !UNITY_WEBPLAYER
 				GestureIO.WriteGesture(points.ToArray(), newGestureName, fileName);
-			#endif
+#endif
 
-			trainingSet.Add(new Gesture(points.ToArray(), newGestureName));
+				trainingSet.Add(new Gesture(points.ToArray(), newGestureName));
 
-			newGestureName = "";
+				newGestureName = "";
+			}
 		}
 	}
 }
